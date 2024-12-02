0f86ed338cf6498944c170950d10a50915a8552e
longgpgp2 <89758492+longgpgp2@users.noreply.github.com>
2024-12-03 00:11:22 +0700

    init project

---
+++ fitconnections/src/main/java/com/hanu/fitconnections/FitConnectionsApplication.java
@@ -0,0 +1,13 @@
+package com.hanu.fitconnections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitConnectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitConnectionsApplication.class, args);
+	}
+
+}
---
+++ fitconnections/src/test/java/com/hanu/fitconnections/FitConnectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanu.fitconnections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitConnectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
